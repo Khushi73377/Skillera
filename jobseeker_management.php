db2425f33aaa3233f2ccc61bdf50129611283017
Khushi73377 <khushikumari73377@gmail.com>
2025-04-06 20:45:38 +0530

    Update jobseeker_management.php

--- jobseeker_management.php
+++ jobseeker_management.php
@@ -51,6 +51,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["action"]) && $_POST["a
             padding: 0;
             display: flex;
             min-height: 100vh;
+            background:url('job7.avif') no-repeat center center/cover;
+
         }
 
         .sidebar {
